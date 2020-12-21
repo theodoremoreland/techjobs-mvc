4da23344cdb165c50e3cbc61459432a218ebbb4d
Theodore Moreland <theodoremoreland@yahoo.com>
2020-12-20 22:40:10 -0600

    Consolidated 2018 and 2019 versions into same rep. This is the initial commit for 2019 branch

---
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -0,0 +1,41 @@
+package org.launchcode.javawebdevtechjobsmvc.controllers;
+
+import org.launchcode.javawebdevtechjobsmvc.models.Job;
+import org.launchcode.javawebdevtechjobsmvc.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
+
+}
